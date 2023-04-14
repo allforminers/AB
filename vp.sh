2a41f71d20c92b02eb281c95efb4877d9ac7f1ee
allforminers <ftresfi@gmail.com>
2023-04-14 21:27:25 +0200

    Update vp.sh

--- vp.sh
+++ vp.sh
@@ -1,80 +1,60 @@
 #!/bin/bash
 
-# Add Redis repository
+# Update package lists and install Redis
 sudo add-apt-repository ppa:chris-lea/redis-server -y
-
-# Update package lists
 sudo apt-get update
+sudo apt install npm -y
+sudo apt -y install redis-server -y
 
-# Install Redis server
-sudo apt-get install redis-server -y
-
-# Update Redis configuration to bind to 127.0.0.1
+# Modify Redis configuration to bind to localhost only
 sudo sed -i 's/bind 127.0.0.1 ::1/bind 127.0.0.1/g' /etc/redis/redis.conf
 
-# Install necessary dependencies for AB
-sudo apt install nodejs node-gyp libssl1.0-dev npm libdb4.8-dev libdb4.8++-dev libevent-dev libgmp-dev -y
+# Install necessary dependencies
+sudo apt install nodejs node-gyp libssl1.0-dev libgmp-dev libevent-dev npm -y
 
-# Install n package for Node.js version management
+# Install n package and Node.js v12
 sudo npm install n -g
-
-# Install Node.js v12 with n
 sudo n v12
 
-# Add Bitcoin repository
-sudo add-apt-repository ppa:bitcoin/bitcoin -y
-
-# Update package lists
+# Install necessary libraries for Bitcoin
+sudo add-apt-repository ppa:bitcoin/bitcoin
 sudo apt-get update
+sudo apt-get install libdb4.8-dev libdb4.8++-dev
 
-# Install necessary dependencies for AB and Bitcoin
-sudo apt-get install libdb4.8-dev libdb4.8++-dev libevent-dev libgmp-dev -y
-
-# Clone AB repository from GitHub
+# Clone AB repository and set permissions
 git clone https://github.com/allforminers/AB.git
-
-# Set permissions for AB folder
 chmod -R 777 AB
 
-# Change directory to AB
+# Copy necessary files to root directory
 cd AB
-
-# Copy necessary files and folders to root directory
 cp -r .aurum/ aurum-cli aurumd aurum-tx aurum-wallet .bitcoin/ bitcoin-cli bitcoind bitcoin-tx CHANGELOG.md .circleci/ config.json docker-compose.yml coins pool_configs scrypt.sh scrypt-all.sh Dockerfile ecosystem.config.js examples/ .git/ .gitattributes init.js libs/ LICENSE package.json README.md scripts/ .travis.yml website/ /root/
-
-# Change directory to root directory
 cd
 
 # Set permissions for root directory
 chmod -R 777 /root
 
-# Remove node_modules folder if it exists
-rm -r node_modules/ || true
+# Remove any existing node_modules and update npm packages
 
-# Update npm
-sudo npm update
+npm update -y
 
-# Install necessary npm packages
-sudo npm install
+npm install -y
 
-# Start Redis server in daemon mode
-redis-server --daemonize yes
 
-# Configure firewall
-sudo apt-get install ipset iptables -y
-sudo ufw --force disable
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo ufw allow ssh
-sudo ufw allow 80/tcp
-sudo ufw allow 17117/tcp
-sudo ufw allow 3008/tcp
-sudo ufw allow 3032/tcp
+# Allow necessary ports through firewall
 sudo ufw allow 3187/tcp
 sudo ufw allow 3188/tcp
 sudo ufw allow 3189/tcp
-sudo ufw allow 3256/tcp
-sudo ufw allow 6379/tcp
 sudo ufw allow 9818/tcp
 sudo ufw allow 21402/tcp
-sudo ufw enable
+sudo ufw allow 3032/tcp
+sudo ufw allow 3256/tcp
+sudo ufw allow 6379/tcp
+sudo ufw allow 3256/tcp
+sudo ufw allow 3008/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 17117/tcp
+sudo ufw allow 6379/tcp
+
+# Disable firewall and allow SSH connections
+sudo ufw --force disable
+sudo ufw allow ssh
